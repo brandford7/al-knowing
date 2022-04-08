3aef6cbab09fb89af9ed3141cafcc96d5ac99791
brandford7 <brandfordjunior@gmail.com>
2022-04-08 16:51:09 +0000

    ga change

--- pages/_app.js
+++ pages/_app.js
@@ -22,11 +22,12 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      >
-        <script
+      <ChakraProvider>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
           id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -39,9 +40,7 @@ function MyApp({ Component, pageProps }) {
             });
           `,
           }}
-        ></script>
-      </Script>
-      <ChakraProvider>
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import * as gtag from "../lib/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
