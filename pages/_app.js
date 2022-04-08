dac8530f88e8a1043ce05c0580975ba7c8930d93
brandford7 <brandfordjunior@gmail.com>
2022-04-08 15:55:33 +0000

    gA fix

--- pages/_app.js
+++ pages/_app.js
@@ -22,8 +22,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      
-   
+      <Script
+        strategy="lazyOnLoad"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
